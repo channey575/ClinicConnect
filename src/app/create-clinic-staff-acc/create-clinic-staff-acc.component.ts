268a1ffed7e21016ba7ce14b2ce3e9d56cf6b8e8
channey575 <68887496+channey575@users.noreply.github.com>
2023-11-23 10:21:25 +0800

    Add files via upload

---
+++ src/app/create-clinic-staff-acc/create-clinic-staff-acc.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-create-clinic-staff-acc',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './create-clinic-staff-acc.component.html',
+  styleUrl: './create-clinic-staff-acc.component.css'
+})
+export class CreateClinicStaffAccComponent {
+
+}
